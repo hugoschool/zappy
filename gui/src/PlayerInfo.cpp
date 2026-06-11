ff92111906ef521026ec8bc842ef9b2187e72ddc
Esteban Hazanas <esteban.hazanas@epitech.eu>
2026-06-11 14:52:00 +0200

    feat: ppo & plv

--- gui/src/PlayerInfo.cpp
+++ gui/src/PlayerInfo.cpp
@@ -5,7 +5,7 @@
 zappy::PlayerInfo::PlayerInfo(int playerNb, tileCoordinates coord,
     int orientation, int level, std::string teamName) : _playerNb(playerNb),
     _pos(coord), _orientation(orientation), _level(level), _teamName(teamName),
-    _inventory()
+    _inventory(), _isEgg(false)
 {}
 
 zappy::PlayerInfo::~PlayerInfo()
@@ -19,4 +19,20 @@ zappy::tileCoordinates zappy::PlayerInfo::getCoords()
 std::string zappy::PlayerInfo::getTeamName()
 {
     return _teamName;
+}
+
+bool zappy::PlayerInfo::isEgg()
+{
+    return _isEgg;
+}
+
+void zappy::PlayerInfo::updatePos(zappy::tileCoordinates pos, int orientation)
+{
+    _pos = pos;
+    _orientation = orientation;
+}
+
+void zappy::PlayerInfo  ::updateLevel(int newLevel)
+{
+    _level = newLevel;
 }
