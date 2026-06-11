5fa90511c6323b3fab453fc1dc4828c8adb06333
Esteban Hazanas <esteban.hazanas@epitech.eu>
2026-06-11 13:53:43 +0200

    fix: pnw & PlayerInfo class

--- gui/include/Player.hpp
+++ gui/include/Player.hpp
@@ -1,13 +1,29 @@
 #pragma once
 
 #include "IEntity.hpp"
+#include "entities/Materials.hpp"
+#include <map>
 #include <string>
+
 namespace zappy {
-    typedef struct {
-        int playerNb;
-        tileCoordinates pos;
-        int orientation;
-        int level;
-        std::string teamName;
-    } PlayerInfo;
+    class PlayerInfo {
+        public:
+            PlayerInfo() = delete;
+            PlayerInfo(int playerNb, tileCoordinates, int orientation, int level, std::string);
+            ~PlayerInfo();
+
+            tileCoordinates getCoords();
+            std::string getTeamName();
+
+        private:
+            int _playerNb;
+            tileCoordinates _pos;
+            int _orientation;
+            int _level;
+            std::string _teamName;
+
+            std::map<MaterialType, int> _inventory;
+
+    };
+
 }
