7d38c733a7faeb0f55395765f48ba92b5b17618d
sacha <sachadef31@gmail.com>
2026-06-16 20:18:54 +0200

    [ADD] Player FOV highlight when selected (might need to change the color)

--- gui/include/Utils.hpp
+++ gui/include/Utils.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "IEntity.hpp"
 #include <string>
 #include <sstream>
 
@@ -22,5 +23,31 @@ namespace zappy {
 
                 return ss.eof();
             }
+
+            static int getOrientation(int orientation) {
+                switch (orientation) {
+                    case 1: return 90;
+                    case 2: return 180;
+                    case 3: return 270;
+                    case 4: return 0;
+                    default: return 0;
+                }
+            };
+
+            static tileCoordinates handleTileOverflow(tileCoordinates coords, std::pair<int, int> dimensions) {
+                if (coords.first >= dimensions.first) {
+                    coords.first = 0;
+                }
+                if (coords.second >= dimensions.second) {
+                    coords.second = 0;
+                }
+                if (coords.first < 0) {
+                    coords.first = dimensions.first - 1;
+                }
+                if (coords.second < 0) {
+                    coords.second = dimensions.second - 1;
+                }
+                return coords;
+            };
     };
 }
