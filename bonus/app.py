679f5d3bffef494bb797b9bb16e0102233281d33
Hector CORDAT-BOURSIAC <hector.cordat-boursiac@epitech.eu>
2026-06-16 10:12:17 +0200

    [FIX] routes

--- bonus/app.py
+++ bonus/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, Response
+from dataclasses import asdict
 from tcp_receiver import start_tcp_server
 from log_buffer import buffer
 
@@ -10,11 +11,18 @@ def index() -> str:
 
 @app.route('/state')
 def state() -> Response:
-    return jsonify(buffer.get_state())
+    data = asdict(buffer.get_state())
+    data["resources"] = {f"{x},{y}": q for (x, y), q in data["resources"].items()}
+    return jsonify(data)
 
 @app.route('/tile/<int:x>/<int:y>')
 def tile(x: int, y: int) -> str:
-    return render_template('tile.html', x=x, y=y, players=buffer.get_tile(x, y))
+    return render_template(
+        'tile.html',
+        x = x, y = y,
+        players = buffer.get_tile(x, y),
+        resources = buffer.resources.get((x, y)),
+    )
 
 if __name__ == '__main__':
     start_tcp_server()
