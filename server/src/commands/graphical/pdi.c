bc5bf2442463137c4b717e23f01df0c0c033d877
Hugo ARNAL <hugo@hugoarnal.com>
2026-06-18 17:24:52 +0200

    fix: correct player number for `pdi`

--- server/src/commands/graphical/pdi.c
+++ server/src/commands/graphical/pdi.c
@@ -4,5 +4,5 @@
 
 void command_graphic_pdi_index(server_t *server, int graphic_i, int player_i)
 {
-    dprintf(*CLIENT_I(graphic_i)->fd, "pdi %d" ZMSG_END_SEQ, player_i);
+    dprintf(*CLIENT_I(graphic_i)->fd, "pdi #%d" ZMSG_END_SEQ, player_i);
 }
