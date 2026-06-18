434322ba62de5feea60c6ddbdb5b8db73a708955
sacha <sachadef31@gmail.com>
2026-06-18 15:55:29 +0200

    [FIX] players orientation

--- gui/include/Utils.hpp
+++ gui/include/Utils.hpp
@@ -36,10 +36,10 @@ namespace zappy {
 
             static int getOrientation(int orientation) {
                 switch (orientation) {
-                    case 1: return 0;
-                    case 2: return 270;
-                    case 3: return 180;
-                    case 4: return 90;
+                    case 1: return 90;
+                    case 2: return 0;
+                    case 3: return 270;
+                    case 4: return 180;
                     default: return 0;
                 }
             }
