6d5ba281491a1e13674e16453c0ee1c083e8526f
Esteban Hazanas <esteban.hazanas@epitech.eu>
2026-06-20 18:14:18 +0200

    feat: the player now is logged in

--- gui/bonus/src/PlayerCommunication.cpp
+++ gui/bonus/src/PlayerCommunication.cpp
@@ -2,11 +2,11 @@
 #include "SafeQueue.hpp"
 #include <iostream>
 #include <string>
+#include <unistd.h>
 
 zappy::PlayerCommunication::PlayerCommunication(int port, std::string hostname, bool &exit, SafeQueue<std::string> &queue) : _communication(port, hostname),
     _safeQueue(queue), _exit(exit), _port(port), _hostname(hostname), _teamName()
 {
-    _communication.sendMessage("team1");
 }
 
 zappy::PlayerCommunication::~PlayerCommunication()
@@ -14,13 +14,16 @@ zappy::PlayerCommunication::~PlayerCommunication()
 
 void zappy::PlayerCommunication::communicationLoop()
 {
+    _communication.sendMessage("team1\n");
     while (!_exit) {
         std::string message("");
 
         if (_safeQueue.tryPop(message)) {
             _communication.sendMessage(message);
         }
-        std::cout << _communication.runSocket(1) << std::endl;
+        std::string msg(_communication.runSocket(1));
+        if (!msg.empty())
+            std::cout << _communication.runSocket(1) << std::endl;
     }
 }
 
