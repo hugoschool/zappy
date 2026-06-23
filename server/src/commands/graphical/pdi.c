ef9bd3b9db345fe830ed97a2ca1e7fa3216b5ead
Bastien Sukiennik <bastien.sukiennik@epitech.eu>
2026-06-23 18:18:38 +0200

    [ADD] missing word in the last commit

--- server/src/commands/graphical/pdi.c
+++ server/src/commands/graphical/pdi.c
@@ -7,7 +7,7 @@ void command_graphic_pdi_index(server_t *server, int graphic_i, int player_i)
 {
     // TODO verfiy this
     // got a valgrind error when triyng to test my widn conditions (invalid read of size 4 and 8)
-    // when ^C a 
+    // when ^C the AI
     printf("%d\n", PLAYER_I(player_i)->player_graphical_index);
     dprintf(*CLIENT_I(graphic_i)->fd, "pdi #%d" ZMSG_END_SEQ, PLAYER_I(player_i)->player_graphical_index);
 }
