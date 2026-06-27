5e6d435b4ff24f1811432a85d8b877158203cd58
Hugo ARNAL <hugo@hugoarnal.com>
2026-06-27 21:09:34 +0200

    fix: only send death message if the client is logged in
    Prevents the server from incorrectly reading from the players array

--- server/src/poll.c
+++ server/src/poll.c
@@ -44,7 +44,7 @@ void client_quit(server_t *server)
     if (fd != server->control_fd && fd != server->signal_fd) {
         if (close(fd) == -1)
             perror("close");
-        if (CLIENT->is_graphical == false)
+        if (CLIENT->is_graphical == false && CLIENT->current_step == LOGGED_IN)
             client_send_death_message(server);
         players_delete(server->players, CLIENT->player_index);
         poller_delete(server->poller, server->index);
